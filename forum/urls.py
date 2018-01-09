ab1fb37ee9c1918af61f99f6bdc4ff98f377719d
Felipe Netto <flpncc@gmail.com>
2018-01-08 21:51:48 -0300

    Making nav more robust

--- forum/urls.py
+++ forum/urls.py
@@ -3,6 +3,8 @@ from django.urls import path
 from . import views
 
 
+app_name = 'forum'
+
 urlpatterns = [
     path('', views.QuestionListView.as_view(), name='questions'),
     path('<slug:slug>/', views.QuestionDetailView.as_view(), name='detail'),
