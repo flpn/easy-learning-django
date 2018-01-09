cfa8bf68606b5c6159df4c4b4fffff7ab284c9c2
Felipe Netto <flpncc@gmail.com>
2018-01-08 21:11:42 -0300

    Creating Question model

--- forum/admin.py
+++ forum/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Question
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    readonly_fields = ('views', 'score')
+
+
+admin.site.register(Question, QuestionAdmin)
--- forum/models.py
+++ forum/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.conf import settings
+from django.db.models.signals import pre_save
 
-# Create your models here.
+from .utils import unique_slug_generator
+
+
+User = settings.AUTH_USER_MODEL
+
+class Question(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=120)
+    text = models.TextField(max_length=500)
+    views = models.IntegerField(default=0)
+    score = models.IntegerField(default=0)
+    tags = models.TextField()
+    published = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    slug = models.SlugField()
+
+    def __str__(self):
+        return self.title
+
+
+def pre_save_receiver(sender, instance, *args, **kwargs):
+    if not instance.slug:
+        instance.slug = unique_slug_generator(instance)
+
+
+pre_save.connect(pre_save_receiver, sender=Question)
